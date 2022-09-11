e4563fc042f9d5447126cdf6522a23a3b8795722
Vy Nguyen <vy1872@gmail.com>
2022-09-11 16:45:25 -0700

    made the connection to the database and the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
+const db =  require(`./config/connection`);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(PORT, () => { console.log(`API server running on port ${PORT}`) });
+db.once(`open`, () => { app.listen(PORT, () => { console.log(`API server running on port ${PORT}`) }) });
