f3e74fa452aab145c8fdb24c084fbd54e4a03a38
Vy Nguyen <vy1872@gmail.com>
2022-09-25 17:11:01 -0700

    add to the user controller to change the user's friends or delete them

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,5 +31,23 @@ module.exports = {
         )
             .then((user) => !user ? res.status(404).json({ message: `No user with that ID`}) : res.json(user))
             .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendsId } },
+            { new: true }
+        )
+            .then((user) => !user ? res.status(404).json({ message: `No user with that ID`}) : res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteFriend(req, res) {
+        User.findOneAndDelete(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendsId } },
+            { new: true }
+        )
+        .then((user) => !user ? res.status(404).json({ message: `No user with that ID`}) : res.json(user))
+        .catch((err) => res.status(500).json(err));
     }
 }
